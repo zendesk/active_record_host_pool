cc941601ed55a7dcad4145095d00982622f6b198
Ben Osheroff <ben@zendesk.com>
2011-04-06 14:47:55 -0700

    bring back delegate.rb, also stub out a bunch of methods here to enable monkey-patching/mocha

--- lib/active_record_host_pool/connection_proxy.rb
+++ lib/active_record_host_pool/connection_proxy.rb
@@ -1,24 +1,26 @@
+require 'delegate'
+
 # the ConnectionProxy sits between user-code and a real connection and says "I expect to be on this database"
 # for each call to the connection.  upon executing a statement, the connection will switch to that database.
 module ActiveRecordHostPool
-  class ConnectionProxy
+  class ConnectionProxy < Delegator
     def initialize(cx, database)
       @cx = cx
       @database = database
     end
 
     def __getobj__
+      @cx._host_pool_current_database = @database
       @cx
     end
 
-    def method_missing(m, *args, &block)
-      target = self.__getobj__
-      begin
-        target._host_pool_current_database = @database
-        target.respond_to?(m) ? target.__send__(m, *args, &block) : super(m, *args, &block)
-      ensure
-        $@.delete_if {|t| %r"\A#{Regexp.quote(__FILE__)}:#{__LINE__-2}:"o =~ t} if $@
-      end
+    # this enables mocha and friends as well as folks who monkey patch execute() to work.
+    ActiveRecord::ConnectionAdapters::AbstractAdapter.instance_methods.each do |method|
+      class_eval <<-EOL
+        def #{method}(*args, &block)
+          method_missing(:#{method}, *args, &block)
+        end
+      EOL
     end
   end
 end
