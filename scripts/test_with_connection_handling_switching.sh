c50160d47f66daabcd1ec1ccc01548199b46285a
Nony Dutton <nony.dutton@zendesk.com>
2022-08-04 13:52:04 +0200

    Update CI

--- scripts/test_with_connection_handling_switching.sh
+++ scripts/test_with_connection_handling_switching.sh
@@ -2,8 +2,8 @@
 
 if [[ $(ruby -r active_record -e 'puts ActiveRecord.version') == 6.1.* ]]
 then
-  LEGACY_CONNECTION_HANDLING=true bundle exec rake test
-  LEGACY_CONNECTION_HANDLING=false bundle exec rake test
+  LEGACY_CONNECTION_HANDLING=true RAILS_ENV=test bundle exec rake test
+  LEGACY_CONNECTION_HANDLING=false RAILS_ENV=test bundle exec rake test
 else
-  bundle exec rake test
+  RAILS_ENV=test bundle exec rake test
 fi
