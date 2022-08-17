b7f80da03e5a5ecffd2aa881935ab6fa2297ec78
Nony Dutton <nony.dutton@zendesk.com>
2022-08-17 10:34:50 +0200

    Re-add eval

--- test/helper.rb
+++ test/helper.rb
@@ -25,108 +25,112 @@ module ARHPTestSetup
     return if ARHPTestSetup.const_defined?('Pool1DbA')
 
     if ActiveRecord.version >= Gem::Version.new('6.1') && !(ENV['LEGACY_CONNECTION_HANDLING'] == 'true')
-      class AbstractPool1DbC < ::ActiveRecord::Base
-        self.abstract_class = true
-        connects_to database: { writing: :test_pool_1_db_c }
-      end
-      class Pool1DbC < AbstractPool1DbC
-      end
+      eval <<-RUBY
+        class AbstractPool1DbC < ::ActiveRecord::Base
+          self.abstract_class = true
+          connects_to database: { writing: :test_pool_1_db_c }
+        end
+        class Pool1DbC < AbstractPool1DbC
+        end
 
-      class AbstractPool1DbA < ActiveRecord::Base
-        self.abstract_class = true
-        connects_to database: { writing: :test_pool_1_db_a, reading: :test_pool_1_db_a_replica }
-      end
+        class AbstractPool1DbA < ActiveRecord::Base
+          self.abstract_class = true
+          connects_to database: { writing: :test_pool_1_db_a, reading: :test_pool_1_db_a_replica }
+        end
 
-      class Pool1DbA < AbstractPool1DbA
-        self.table_name = "tests"
-      end
+        class Pool1DbA < AbstractPool1DbA
+          self.table_name = "tests"
+        end
 
-      class AbstractPool1DbB < ActiveRecord::Base
-        self.abstract_class = true
-        connects_to database: { writing: :test_pool_1_db_b }
-      end
+        class AbstractPool1DbB < ActiveRecord::Base
+          self.abstract_class = true
+          connects_to database: { writing: :test_pool_1_db_b }
+        end
 
-      class Pool1DbB < AbstractPool1DbB
-        self.table_name = "tests"
-      end
+        class Pool1DbB < AbstractPool1DbB
+          self.table_name = "tests"
+        end
 
-      class AbstractShardedModel < ActiveRecord::Base
-        self.abstract_class = true
-        connects_to shards: {
-                      default: { writing: :test_pool_1_db_shard_a },
-                      shard_b: { writing: :test_pool_1_db_shard_b },
-                      shard_c: { writing: :test_pool_1_db_shard_c },
-                      shard_d: { writing: :test_pool_2_db_shard_d }
-                    }
-      end
+        class AbstractShardedModel < ActiveRecord::Base
+          self.abstract_class = true
+          connects_to shards: {
+                        default: { writing: :test_pool_1_db_shard_a },
+                        shard_b: { writing: :test_pool_1_db_shard_b },
+                        shard_c: { writing: :test_pool_1_db_shard_c },
+                        shard_d: { writing: :test_pool_2_db_shard_d }
+                      }
+        end
 
-      class ShardedModel < AbstractShardedModel
-        self.table_name = "tests"
-      end
+        class ShardedModel < AbstractShardedModel
+          self.table_name = "tests"
+        end
 
-      class AbstractPool2DbD < ActiveRecord::Base
-        self.abstract_class = true
-        connects_to database: { writing: :test_pool_2_db_d }
-      end
+        class AbstractPool2DbD < ActiveRecord::Base
+          self.abstract_class = true
+          connects_to database: { writing: :test_pool_2_db_d }
+        end
 
-      class Pool2DbD < AbstractPool2DbD
-        self.table_name = "tests"
-      end
+        class Pool2DbD < AbstractPool2DbD
+          self.table_name = "tests"
+        end
 
-      class AbstractPool2DbE < ActiveRecord::Base
-        self.abstract_class = true
-        connects_to database: { writing: :test_pool_2_db_e }
-      end
+        class AbstractPool2DbE < ActiveRecord::Base
+          self.abstract_class = true
+          connects_to database: { writing: :test_pool_2_db_e }
+        end
 
-      class Pool2DbE < AbstractPool2DbE
-        self.table_name = "tests"
-      end
+        class Pool2DbE < AbstractPool2DbE
+          self.table_name = "tests"
+        end
 
-      class AbstractPool3DbE < ActiveRecord::Base
-        self.abstract_class = true
-        connects_to database: { writing: :test_pool_3_db_e }
-      end
+        class AbstractPool3DbE < ActiveRecord::Base
+          self.abstract_class = true
+          connects_to database: { writing: :test_pool_3_db_e }
+        end
 
-      class Pool3DbE < AbstractPool3DbE
-        self.table_name = "tests"
-      end
+        class Pool3DbE < AbstractPool3DbE
+          self.table_name = "tests"
+        end
+      RUBY
     else
-      # The placement of the Pool1DbC class is important so that its
-      # connection will not be the most recent connection established
-      # for test_pool_1.
-      class Pool1DbC < ::ActiveRecord::Base
-        establish_connection(:test_pool_1_db_c)
-      end
+      eval <<-RUBY
+        # The placement of the Pool1DbC class is important so that its
+        # connection will not be the most recent connection established
+        # for test_pool_1.
+        class Pool1DbC < ::ActiveRecord::Base
+          establish_connection(:test_pool_1_db_c)
+        end
 
-      class Pool1DbA < ActiveRecord::Base
-        self.table_name = "tests"
-        establish_connection(:test_pool_1_db_a)
-      end
+        class Pool1DbA < ActiveRecord::Base
+          self.table_name = "tests"
+          establish_connection(:test_pool_1_db_a)
+        end
 
-      class Pool1DbAReplica < ActiveRecord::Base
-        self.table_name = "tests"
-        establish_connection(:test_pool_1_db_a_replica)
-      end
+        class Pool1DbAReplica < ActiveRecord::Base
+          self.table_name = "tests"
+          establish_connection(:test_pool_1_db_a_replica)
+        end
 
-      class Pool1DbB < ActiveRecord::Base
-        self.table_name =  "tests"
-        establish_connection(:test_pool_1_db_b)
-      end
+        class Pool1DbB < ActiveRecord::Base
+          self.table_name =  "tests"
+          establish_connection(:test_pool_1_db_b)
+        end
 
-      class Pool2DbD < ActiveRecord::Base
-        self.table_name = "tests"
-        establish_connection(:test_pool_2_db_d)
-      end
+        class Pool2DbD < ActiveRecord::Base
+          self.table_name = "tests"
+          establish_connection(:test_pool_2_db_d)
+        end
 
-      class Pool2DbE < ActiveRecord::Base
-        self.table_name = "tests"
-        establish_connection(:test_pool_2_db_e)
-      end
+        class Pool2DbE < ActiveRecord::Base
+          self.table_name = "tests"
+          establish_connection(:test_pool_2_db_e)
+        end
 
-      class Pool3DbE < ActiveRecord::Base
-        self.table_name = "tests"
-        establish_connection(:test_pool_3_db_e)
-      end
+        class Pool3DbE < ActiveRecord::Base
+          self.table_name = "tests"
+          establish_connection(:test_pool_3_db_e)
+        end
+      RUBY
     end
   end
 
