dc600f39189fe9aedeb444d6f9f4f2a03b42661a
Benjamin Quorning <benjamin@quorning.net>
2022-08-20 11:59:00 +0200

    fixup! Add tests for Rails 6.1's new connection handling

--- test/test_arhp_with_new_connection_handling.rb
+++ test/test_arhp_with_new_connection_handling.rb
@@ -48,49 +48,45 @@ if ActiveRecord.version >= Gem::Version.new('6.1') && ENV['LEGACY_CONNECTION_HAN
         ShardedModel.create!
       end
 
-      assert_equal (AbstractShardedModel.connected_to(role: :writing, shard: :shard_b) do
-                      ShardedModel.count
-                    end), 1
+      records_on_shard_b = AbstractShardedModel.connected_to(role: :writing, shard: :shard_b) do
+        ShardedModel.count
+      end
+      records_on_shard_d = AbstractShardedModel.connected_to(role: :writing, shard: :shard_d) do
+        ShardedModel.count
+      end
 
-      assert_equal (AbstractShardedModel.connected_to(role: :writing, shard: :shard_d) do
-                      ShardedModel.count
-                    end), 1
-
-      assert_equal ShardedModel.count, 0
+      assert_equal 1, records_on_shard_b
+      assert_equal 1, records_on_shard_d
+      assert_equal 0, ShardedModel.count
     end
 
     def test_shards_with_matching_hosts_ports_sockets_usernames_and_replica_status_should_share_a_connection
       default_shard_connection = ShardedModel.connection.raw_connection
-      pool_1_shard_b_writing_connection = (AbstractShardedModel.connected_to(role: :writing, shard: :shard_b) do
-                                             ShardedModel.connection.raw_connection
-                                           end)
-
-      pool_1_shard_b_reading_connection = (AbstractShardedModel.connected_to(role: :reading, shard: :shard_b) do
-                                             ShardedModel.connection.raw_connection
-                                           end)
-
-      pool_1_shard_c_reading_connection = (AbstractShardedModel.connected_to(role: :reading, shard: :shard_c) do
-                                             ShardedModel.connection.raw_connection
-                                           end)
+      pool_1_shard_b_writing_connection = AbstractShardedModel.connected_to(role: :writing, shard: :shard_b) do
+        ShardedModel.connection.raw_connection
+      end
+      pool_1_shard_b_reading_connection = AbstractShardedModel.connected_to(role: :reading, shard: :shard_b) do
+        ShardedModel.connection.raw_connection
+      end
+      pool_1_shard_c_reading_connection = AbstractShardedModel.connected_to(role: :reading, shard: :shard_c) do
+        ShardedModel.connection.raw_connection
+      end
 
       assert_equal(default_shard_connection, pool_1_shard_b_writing_connection)
       assert_equal(pool_1_shard_b_reading_connection, pool_1_shard_c_reading_connection)
     end
 
     def test_shards_without_matching_ports_should_not_share_a_connection
-      refute_equal(ShardedModel.connection.raw_connection,
-                   (AbstractShardedModel.connected_to(role: :writing, shard: :shard_d) do
-                      ShardedModel.connection.raw_connection
-                    end))
+      default_shard_connection = ShardedModel.connection.raw_connection
+      pool_1_shard_b_writing_connection = AbstractShardedModel.connected_to(role: :writing, shard: :shard_b) do
+        ShardedModel.connection.raw_connection
+      end
+      pool_2_shard_d_writing_connection = AbstractShardedModel.connected_to(role: :writing, shard: :shard_d) do
+        ShardedModel.connection.raw_connection
+      end
 
-      refute_equal(
-        (AbstractShardedModel.connected_to(role: :writing, shard: :shard_b) do
-           ShardedModel.connection.raw_connection
-         end),
-        ((AbstractShardedModel.connected_to(role: :writing, shard: :shard_d) do
-            ShardedModel.connection.raw_connection
-          end))
-      )
+      refute_equal(default_shard_connection, pool_2_shard_d_writing_connection)
+      refute_equal(pool_1_shard_b_writing_connection, pool_2_shard_d_writing_connection)
     end
 
     def test_reading_and_writing_roles_should_not_share_a_connection
@@ -101,17 +97,23 @@ if ActiveRecord.version >= Gem::Version.new('6.1') && ENV['LEGACY_CONNECTION_HAN
     end
 
     def test_sharded_reading_and_writing_roles_should_not_share_a_connection
-      refute_equal(
-        (AbstractShardedModel.connected_to(role: :writing, shard: :shard_c) { ShardedModel.connection.raw_connection }),
-        (AbstractShardedModel.connected_to(role: :reading, shard: :shard_c) { ShardedModel.connection.raw_connection })
-      )
+      shard_c_writing_connection = AbstractShardedModel.connected_to(role: :writing, shard: :shard_c) do
+        ShardedModel.connection.raw_connection
+      end
+      shard_c_reading_connection = AbstractShardedModel.connected_to(role: :reading, shard: :shard_c) do
+        ShardedModel.connection.raw_connection
+      end
+
+      refute_equal(shard_c_writing_connection, shard_c_reading_connection)
     end
 
     def test_sharded_reading_roles_without_matching_ports_should_not_share_a_connection
-      shard_c_reading_connection =
-        (AbstractShardedModel.connected_to(role: :reading, shard: :shard_c) { ShardedModel.connection.raw_connection })
-      shard_d_reading_connection =
-        (AbstractShardedModel.connected_to(role: :reading, shard: :shard_d) { ShardedModel.connection.raw_connection })
+      shard_c_reading_connection = AbstractShardedModel.connected_to(role: :reading, shard: :shard_c) do
+        ShardedModel.connection.raw_connection
+      end
+      shard_d_reading_connection = AbstractShardedModel.connected_to(role: :reading, shard: :shard_d) do
+        ShardedModel.connection.raw_connection
+      end
 
       refute_equal(shard_c_reading_connection, shard_d_reading_connection)
     end
