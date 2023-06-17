6a045f1960c2098ac5b05b40494b295f164e3703
Nony Dutton <nony.dutton@zendesk.com>
2023-06-17 14:20:37 +0200

    Move development dependencies to common gemfile
    We've decided to move development dependencies from the gemspec to
    the (common) gemfile. Occasionally development dependencies (for one
    reason or another) need to be define in the common gemfile so having
    them in two places gets confusing.

--- gemfiles/common.rb
+++ gemfiles/common.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
 gem "byebug", platform: :mri
+gem "minitest", ">= 5.10.0"
+gem "minitest-fail-fast"
+gem "minitest-line"
+gem "minitest-mock_expectations", "~> 1.1.3"
+gem "phenix", ">= 1.0.1"
+gem "pry-byebug", "~> 3.9"
+gem "rake", '>= 12.0.0'
+gem 'rubocop', '~> 0.80.0'
